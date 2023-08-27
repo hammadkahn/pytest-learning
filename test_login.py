db336f1aa73427942c462b20f2a91b024f2cbf63
Hammad Khan <hammad.khan0312@gmail.com>
2023-08-28 01:03:55 +0500

    first srcipt

--- test_login.py
+++ test_login.py
@@ -1,6 +1,14 @@
+import pytest
+
+
 def testlogin():
     print("Login Successfull")
+
+
 def testlogout():
     print("Logout Successfull")
+
+
+@pytest.mark.sanity
 def testcalc():
-    assert 100+200 == 3001
+    assert 100 + 200 == 3001
